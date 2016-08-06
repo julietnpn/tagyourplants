83d8641723e8e5b3ec2960f3f8f5cb785ecdee24
shannonhu519 <shannonhu519@gmail.com>
2016-08-06 08:09:14 -0700

    Adjust the model and view for resultplant list

--- plants/views.py
+++ plants/views.py
@@ -18,11 +18,38 @@ def search_form(request):
 def result_list(request):
     if('q' in request.GET):
         query = request.GET['q']
-        plantResult = Plant.objects.filter(Q(common_name__icontains=query) | Q(scientific_name__icontains=query))
-        search_id = set()
-        for plant in plantResult:
-            search_id.add(getattr(plant, 'plant_id'))
-        return TemplateResponse(request,'plants/resultList.html', {'plantResult': plantResult, 'query': query})
+        postResult = PlantPost.objects.filter(Q(plant__common_name__icontains=query) | Q(plant__scientific_name__icontains=query)).order_by('-score')
+
+        topResult_set = list()
+        topPlant_set = set()
+        for top in postResult:
+            if (getattr(top, 'plant_id')) in topPlant_set:
+                continue
+            else:
+                topPlant_set.add(getattr(top, 'plant_id'))
+        #get the each plant_id
+        for plant in topPlant_set:
+            if plant:
+                topPlant = PlantPost.objects.filter(plant__plant_id__icontains=plant).order_by('-score')
+                r = list(topPlant[:1])
+                if r:
+                    top_post = r[0]
+                    topResult_set.append(getattr(top_post, 'post_id'))
+                else:
+                    top_post = None
+
+        coverItem = PlantPost.objects.filter(pk__in = topResult_set)
+
+        context = {
+            'topPlant_set': topPlant_set,
+            'query': query,
+            'postResult': postResult,
+            'top_post': top_post,
+            'topResult_set': topResult_set,
+            'coverItem': coverItem,
+            'r': r,
+            }
+        return TemplateResponse(request,'plants/resultList.html', context)
     else:
         return HttpResponse(message)
 
@@ -47,5 +74,9 @@ def get_plant(request):
     context = {
         "postResult": postResult,
         "plantResult": plantResult,
+        "top_post": top_post,
     }
     return TemplateResponse(request, 'plants/plantResults.html', context)
+
+
+# def get_post_by_tag(request):
