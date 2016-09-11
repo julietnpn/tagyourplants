b01a9ddc9a809a95c0b755e2b9e5b908a392925a
phatfathu <shannonhu519@gmail.com>
2016-09-11 16:56:22 -0700

    (No setting updates) Added a function in setting.py to allow run locally and at heroku, and collectstatic file.

--- plantySite/settings.py
+++ plantySite/settings.py
@@ -84,7 +84,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'plants/static'),
 )
 
 # Simplified static file serving.
