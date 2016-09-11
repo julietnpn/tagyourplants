730b886afb82461452267053644246561e7fad41
phatfathu <shannonhu519@gmail.com>
2016-09-11 08:00:23 -0700

    Delete the previous DATABASE Definition.

--- plantySite/settings.py
+++ plantySite/settings.py
@@ -105,17 +105,17 @@ WSGI_APPLICATION = 'plantySite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'PlantyPicDB',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'TIME_ZONE': None,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'PlantyPicDB',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'TIME_ZONE': None,
+#     }
+# }
 
 
 # Password validation
